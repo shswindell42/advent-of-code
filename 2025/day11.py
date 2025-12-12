0439408b47fe1463323e2954b578040bf75b5118
Spencer Swindell <spencer.swindell@gmail.com>
2025-12-11 22:48:09 -0600

    solution for 2025 day 11 part 2

--- 2025/day11.py
+++ 2025/day11.py
@@ -1,3 +1,20 @@
+from functools import cache
+
+@cache
+def dfs(server, has_dac, has_fft):
+    connections = graph[server]
+    path_count = 0
+    for c in connections:
+        if c == "out":
+            if has_dac and has_fft:
+                return 1
+            else:
+                return 0
+        else:
+            new_has_dac = has_dac or c == "dac"
+            new_has_fft = has_fft or c == "fft"
+            path_count += dfs(c, new_has_dac, new_has_fft)
+    return path_count
 
 graph = {}
 with open("./2025/day11.txt", "r") as fp:
@@ -7,17 +24,4 @@ with open("./2025/day11.txt", "r") as fp:
         connections = parts[1:]
         graph[key] = connections
         
-visited_servers = set()
-servers = ["you"]
-path_count = 0
-while servers:
-    server = servers.pop()
-    
-    connections = graph[server]
-    for c in connections:
-        if c == "out":
-            path_count += 1
-        else:
-            servers.append(c)
-
-print(path_count)
+print(dfs("svr", False, False))
